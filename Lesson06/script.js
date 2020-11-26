b48c2396421d33cee8d3f4495b9bc099d0a2df97
Activx-nV <68726573+Activx-nV@users.noreply.github.com>
2020-11-26 14:08:14 +0200

    feat: make basic functional

--- Lesson06/script.js
+++ Lesson06/script.js
@@ -5,32 +5,58 @@ function randomInteger(min, max) {
     return Math.floor(random);
 }
 
-let luckyNumber = randomInteger(1, 101);
-let guess;
 
-/*Comment after Tests*/alert(`Lucky number: ${luckyNumber} (For test purposes)`);
+
+/*Comment after Tests*/
 function guessNumber() {
-    
-    guess = prompt('Угадай число от 1 до 100');
+    let guess;
+    let luckyNumber = randomInteger(1, 100);
+    let counter = 9;
+    function gameStart() {
+        guess = prompt('Угадай число от 1 до 100');
 
     if (parseInt(guess) > luckyNumber && typeof parseInt(guess) === 'number') {
-        alert('Загаданное число меньше');
-        guessNumber();
+        alert(`Загаданное число меньше, осталось попыток: ${counter}`);
+        if (counter === 0) {
+            let question = confirm('Попытки закончились, хотите сыграть еще?');
+            if (question) {
+            counter = 9;
+            gameStart();
+            luckyNumber = randomInteger(1, 100);
+        } 
+    } else {
+        --counter;
+    }
+    gameStart();
     } else if (parseInt(guess) < luckyNumber && typeof parseInt(guess) === 'number') {
-        alert('Загаданное число больше');
-        guessNumber();
+        alert(`Загаданное число больше, осталось попыток: ${counter}`);
+        if (counter === 0) {
+            let question = confirm('Попытки закончились, хотите сыграть еще?');
+            if (question) {
+                counter = 9;
+                gameStart();
+            luckyNumber = randomInteger(1, 100);
+        }
+    } else {
+        --counter;
+    }
+    gameStart();
     } else if (guess === null) {
-        alert('Игра окончена');
+        alert('Игра окончена :(');
     } else if (isNaN(guess)) {
         alert('Введи число!');
-        guessNumber();
+        gameStart();
     }  else if (parseInt(guess) === luckyNumber) {
         alert('Поздравляю, Вы угадали!!!');
-       let question = confirm('Хотите сыграть еще раз?');
+       let question = confirm('Хотите сыграть еще?');
        if (question) {
-        guessNumber();
+        counter = 9;
         luckyNumber = randomInteger(1, 100);
+        gameStart();
        }
     }
+    }
+    gameStart();
 }
+
 guessNumber();
